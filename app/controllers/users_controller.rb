4a2b743a041aeeea3cbcdf789810c1ed776819c2
audipod <146144639+audipod@users.noreply.github.com>
2023-11-09 02:15:13 +0000

    let's gooooo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,45 @@ class UsersController < ApplicationController
    # if @the_user == nil
     # redirect_to("/404")
    # else
+
     render({ :template => "user_templates/show" })
+    
    # end
   end
+
+  def create
+
+    input_user = params.fetch("query_username")
+   
+    a_new_user = User.new
+  
+    a_new_user.username = input_user
+  
+    a_new_user.save
+  
+  
+      #render({ :template => "photo_templates/create" })
+  
+      redirect_to("/users/" + a_new_user.username.to_s)
+  
+    end
+
+    def update
+      the_username = params.fetch("modify_id")
+  
+      matching_users = User.where({ :username => the_username })
+  
+      the_user = matching_users.at(0)
+  
+      input_user = params.fetch("query_username")
+  
+      the_user.username = input_user
+  
+      the_user.save
+  
+      #render({ :template => "photo_templates/update" })
+      
+      redirect_to("/users/" + the_user.username.to_s)
+  
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+
   get("/users", { :controller => "users", :action => "index" })
 
   get("/users/:path_username", { :controller => "users", :action => "show"})
@@ -11,4 +13,10 @@ Rails.application.routes.draw do
   get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy"})
 
   post("/insert_photo", { :controller => "photos", :action => "create" })
+
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
+
+  post("/insert_user", { :controller => "users", :action => "create" })
+
+  post("/update_user/:modify_id", { :controller => "users", :action => "update" })
 end
